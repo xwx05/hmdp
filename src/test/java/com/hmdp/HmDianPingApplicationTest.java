25137e56fa59a22dd6911e3d23241d8d4e5c80db
xiaowenxin <xiaowx05@163.com>
2025-07-22 09:34:45 +0800

    学习HyperLogLog性能

--- src/test/java/com/hmdp/HmDianPingApplicationTest.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTest.java
@@ -172,4 +172,18 @@ public class HmDianPingApplicationTest {
 
     }
 
+    @Test
+    public void testHyperLogLog () {
+        String[] values = new String[1000];
+        int j = 0;
+        for (int i= 0; i<1000000; i++) {
+            j = i % 1000;
+            values[j] = "user_" + i;
+            if (j == 999) {
+                stringRedisTemplate.opsForHyperLogLog().add("hl1", values);
+            }
+        }
+        Long count = stringRedisTemplate.opsForHyperLogLog().size("hl1");
+        System.out.println("count=" + count);
+    }
 }
